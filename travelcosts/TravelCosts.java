991e3f360d8af421d94542970b1bd59ff4f2e976
HugoCapitan <hmoravilez@gmail.com>
2018-04-03 11:41:05 -0500

    A string

--- travelcosts/TravelCosts.java
+++ travelcosts/TravelCosts.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class TravelCosts {
   public static void main(String[] args) {
     double distance, milesPerGalon, pricePerGalon, totalCost;
+    String vehicleType;
     Scanner in = new Scanner(System.in);
 
     System.out.println("Enter the total distance in miles");
@@ -12,9 +13,11 @@ public class TravelCosts {
     milesPerGalon = in.nextDouble();
     System.out.println("Enter the price of one gallon of gas");
     pricePerGalon = in.nextDouble();
+    System.out.println("Please enter the vehicle type");
+    vehicleType = in.next();
 
     totalCost = distance / milesPerGalon * pricePerGalon;
-    System.out.printf("The trip is goinf to cost $%5.2f", totalCost);
+    System.out.printf("The trip is goinf to cost $%5.2f since you are driving a " + vehicleType, totalCost);
     System.out.println("");
   }
 }
