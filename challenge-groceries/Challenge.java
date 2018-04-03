f3b581ba8d968396fc1f19fc0cd48e91d2170fe1
HugoCapitan <hmoravilez@gmail.com>
2018-04-03 16:31:44 -0500

    sorting!

--- challenge-groceries/Challenge.java
+++ challenge-groceries/Challenge.java
@@ -15,25 +15,70 @@ public class Challenge {
     groceries.add("Meat");
     groceries.add("Coffee");
     groceries.add("Bread");
+    groceries.add("Salties");
     groceries.add("Apples");
     groceries.add("Wine");
 
     sort(groceries);
 
-    Scanner in = new Scanner(System.in);
+    System.out.println(groceries);
 
-    System.out.println("What do you want to search for?");
-    query = in.next();
+    // System.out.println("What do you want to search for?");
+    // query = in.next();
 
-    search(groceries, search)
+    // search(groceries, search)
   }
 
-  public void sort(ArrayList<String> groceries) {
+  public static void sort(ArrayList<String> groceries) {
+    int smaller, i, j;
+    String temp;
 
+    for (i = 0; i < groceries.size(); i++) {
+      smaller = i;
+      for (j = groceries.size() - 1; j > i; j--) {
+        if ( isMinor(groceries.get(j), groceries.get(smaller)) )
+          smaller = j;
+      }
+      temp = groceries.get(smaller);
+      groceries.set(smaller, groceries.get(i));
+      groceries.set(i, temp);
+    }
   }
 
   public void search(ArrayList<String> groceries, String query) {
     
   }
 
+  public static boolean isMinor(String firstWord, String secondWord) {
+    boolean isMinor = false;
+    boolean again = false;
+    int i = 0;
+
+    do {
+      again = false;
+      if (i == firstWord.length() - 1) {
+        isMinor = true;
+        break;
+      } else if (i == secondWord.length() - 1) {
+        isMinor = false;
+        break;
+      }
+
+      char fc = firstWord.charAt(i);
+      char sc = secondWord.charAt(i);
+      Character.toLowerCase(fc);
+      Character.toLowerCase(sc);
+
+      if (fc == sc)
+        again = true;
+      else if (fc < sc)
+        isMinor = true;
+      else if (fc > sc)
+        isMinor = false;
+      
+      i++;
+    } while (again);
+
+    return isMinor;
+  }
 }
